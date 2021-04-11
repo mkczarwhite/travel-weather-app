2f87139a9d5459817212dc273f146ca092285be4
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-04-11 12:01:11 -0700

    Updated date & time

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+function formatDate (timestamp){
+    let date=new Date(timestamp);
+    let hours=date.getHours();
+    let minutes=date.getMinutes();
+    if (minutes<10){
+        minutes=`0${minutes}`;
+    }
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day=days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement=document.querySelector("#current-temperature");
     let cityElement=document.querySelector("#city");
     let descriptionElement=document.querySelector("#description");
     let windSpeed=document.querySelector("#speed");
+    let dateElement=document.querySelector("#date");
+    dateElement.innerHTML=formatDate(response.data.dt*1000);
     windSpeed.innerHTML=Math.round(response.data.wind.speed);
     descriptionElement.innerHTML=response.data.weather[0].description;
     cityElement.innerHTML= response.data.name;
