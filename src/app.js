c643d86d124afda40d94af50f75fe22fadec738f
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-04-11 11:50:06 -0700

    Connected weather elements
    Connected weather data from API into app utilizing JS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 function displayTemperature(response){
     console.log(response.data);
+    let temperatureElement=document.querySelector("#current-temperature");
+    let cityElement=document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    let windSpeed=document.querySelector("#speed");
+    windSpeed.innerHTML=Math.round(response.data.wind.speed);
+    descriptionElement.innerHTML=response.data.weather[0].description;
+    cityElement.innerHTML= response.data.name;
+    temperatureElement.innerHTML=Math.round(response.data.main.temp);
 }
 let apiKey="d3c980000de9297354460be9460728c9";
 let city="New York";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
