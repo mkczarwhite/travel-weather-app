ae33a9722b0e634b437f4029ac26f76401cd4532
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-04-18 16:07:06 -0700

    Loop through forecast varibles
    get index of each function to format the day, formatting with date stamp, then also including the weather image and rounding the temperature. limited number of days showing.

--- src/app.js
+++ src/app.js
@@ -11,19 +11,31 @@ function formatDate (timestamp){
 
 }
 
+function formatDay(timestamp){
+    let date= new Date(timestamp*1000);
+    let day=date.getDay();
+    let days =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+
+}
+
 function displayForecast(response){
-    console.log(response.data.daily);
+let forecast=response.data.daily;
 let forecastElement=document.querySelector("#forecast");
 let forecastHTML=`<div class="row">`;
-let days=["Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
-days.forEach(function(day){
-forecastHTML= forecastHTML + `
+forecast.forEach(function(forecastDay, index){
+    if (index < 6){
+      forecastHTML= forecastHTML + `
     <div class="col-sm-2">
-       <div class="weather-forecast-date">${day}</div>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="clouds" />
-        <span class="weather-forecast-temperature-max">52º</span> / <span class="weather-forecast-temperature-min">38º</span>
-    </div>`;
-})
+       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+        alt="" />
+        <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}º</span> / <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}º</span>
+    </div>`;  
+    };
+
+});
 
 forecastHTML= forecastHTML + `</div>`;
 forecastElement.innerHTML= forecastHTML;
